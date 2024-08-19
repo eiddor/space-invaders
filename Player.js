e0e7d38a988d409eb453d92185603d0c89ce6023
Roddie Hasan <roddie@krweb.net>
2024-08-19 14:14:02 -0500

    Initial commit

---
+++ Player.js
@@ -0,0 +1,16 @@
+export default class Player {
+  constructor(canvas, velocity) {
+    this.canvas = canvas;
+    this.velocity = velocity;
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height - 75;
+    this.width = 50;
+    this.height = 48;
+    this.image = new Image();
+    this.image.src = "images/player.png";
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
