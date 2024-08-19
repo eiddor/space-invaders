482c229f05965df2f29c4999641b46359ce0b082
Roddie Hasan <roddie@krweb.net>
2024-08-19 14:35:32 -0500

    Player movement

--- Player.js
+++ Player.js
@@ -1,4 +1,7 @@
 export default class Player {
+  rightPressed = false;
+  leftPressed = false;
+
   constructor(canvas, velocity) {
     this.canvas = canvas;
     this.velocity = velocity;
@@ -8,9 +11,51 @@ export default class Player {
     this.height = 48;
     this.image = new Image();
     this.image.src = "images/player.png";
+
+    document.addEventListener("keydown", this.keydown);
+    document.addEventListener("keyup", this.keyup);
   }
 
   draw(ctx) {
+    this.move();
+    this.collideWithWalls();
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
+
+  collideWithWalls() {
+    // left wall
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    // right wall
+    if (this.x > this.canvas.width - this.width) {
+      this.x = this.canvas.width - this.width;
+    }
+  }
+
+  move() {
+    if (this.rightPressed) {
+      this.x += this.velocity;
+    } else if (this.leftPressed) {
+      this.x += -this.velocity;
+    }
+  }
+
+  keydown = (event) => {
+    if (event.code == "ArrowRight") {
+      this.rightPressed = true;
+    }
+    if (event.code == "ArrowLeft") {
+      this.leftPressed = true;
+    }
+  };
+
+  keyup = (event) => {
+    if (event.code == "ArrowRight") {
+      this.rightPressed = false;
+    }
+    if (event.code == "ArrowLeft") {
+      this.leftPressed = false;
+    }
+  };
 }
