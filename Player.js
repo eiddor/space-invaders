949bc1335e559e912d7493459437eb4e87b51805
Roddie Hasan <roddie@krweb.net>
2024-08-19 15:24:07 -0500

    chore: Add bullet shooting functionality to Player class

--- Player.js
+++ Player.js
@@ -1,10 +1,13 @@
 export default class Player {
   rightPressed = false;
   leftPressed = false;
+  shootPressed = false;
 
-  constructor(canvas, velocity) {
+  constructor(canvas, velocity, bulletController) {
     this.canvas = canvas;
     this.velocity = velocity;
+    this.bulletController = bulletController;
+
     this.x = this.canvas.width / 2;
     this.y = this.canvas.height - 75;
     this.width = 50;
@@ -17,6 +20,9 @@ export default class Player {
   }
 
   draw(ctx) {
+    if (this.shootPressed) {
+      this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+    }
     this.move();
     this.collideWithWalls();
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -48,6 +54,9 @@ export default class Player {
     if (event.code == "ArrowLeft") {
       this.leftPressed = true;
     }
+    if (event.code == "Space") {
+      this.shootPressed = true;
+    }
   };
 
   keyup = (event) => {
@@ -56,6 +65,9 @@ export default class Player {
     }
     if (event.code == "ArrowLeft") {
       this.leftPressed = false;
+    }
+    if (event.code == "Space") {
+      this.shootPressed = false;
     }
   };
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import EnemyController from "./EnemyController.js";
 import Player from "./Player.js";
+import BulletController from "./BulletController.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,8 +11,9 @@ canvas.height = 600;
 const background = new Image();
 background.src = "images/space.png";
 
+const playerBulletController = new BulletController(canvas, 10, "red", true);
 const enemyController = new EnemyController(canvas);
-const player = new Player(canvas, 3);
+const player = new Player(canvas, 3, playerBulletController);
 
 function game() {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
